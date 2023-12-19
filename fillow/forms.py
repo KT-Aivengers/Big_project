2d53f9929f21edf5aa4ea724aefe7cd3df910d55
sungup <wsup97@gmail.com>
2023-12-19 16:24:27 +0900

    [BE] Set: models.py 수정(사용자 DB 수정), 회원가입 페이지 수정

--- fillow/forms.py
+++ fillow/forms.py
@@ -4,4 +4,36 @@ from .models import User
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['user_id', 'user_pw', 'user_email', 'user_dep']
+        fields = ['user_name', 'user_id', 'user_pw', 'user_email', 'user_dep', 'user_phone']
+        widgets = {
+            'user_name': forms.TextInput(
+                attrs={
+                    "class":'form-control'
+                }
+            ),
+            'user_id': forms.TextInput(
+                attrs={
+                    "class":'form-control'
+                }
+            ),
+            'user_pw': forms.PasswordInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'user_email': forms.EmailInput(
+                attrs={
+                    "class":'form-control'
+                }
+            ),
+            'user_dep': forms.TextInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'user_phone': forms.TextInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+        }
