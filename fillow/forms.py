3b58a0b01cb6b87accc2f1490aa76e96924c7d99
sungup <wsup97@gmail.com>
2023-12-20 15:08:45 +0900

    [BE] forms 수정

--- fillow/forms.py
+++ fillow/forms.py
@@ -22,27 +22,6 @@ class LoginForm(forms.Form):
     )
     
 
-class LoginForm(forms.Form):
-    username = forms.CharField(
-        label="아이디",
-        widget = forms.TextInput(
-            attrs={
-                "class": 'form-control',
-                "placeholder": "아이디를 입력하세요"  
-            }
-        ),
-    )
-    password = forms.CharField(
-        label="비밀번호",
-        widget = forms.PasswordInput(
-            attrs={
-                "class": 'form-control',
-                "placeholder": "비밀번호를 입력하세요",
-            }
-        ),
-    )
-    
-
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
