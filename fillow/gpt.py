27b7cb55d7ace5f70e7fcb1d5565c693360b0a9c
parlyresk <parlyresk325@gmail.com>
2023-12-22 11:00:02 +0900

    [AI] gpt ocr 부분 넣어서 공백처리 해둠 쓸지 말지는 추후 결정

--- fillow/gpt.py
+++ fillow/gpt.py
@@ -5,7 +5,8 @@ from langchain.prompts import ChatMessagePromptTemplate, ChatPromptTemplate
 from langchain.schema import BaseOutputParser
 import openai,os
 from dotenv import load_dotenv
-
+import base64
+import requests
 
 load_dotenv()
 
@@ -123,3 +124,49 @@ chain = final_prompt | chat | EmailOutputParser()
 def process_file(text):
     a=chain.invoke({"email": text,"language":"Korean","category":category})
     print(a)
+
+
+
+
+# 이미지 인식하는 부분 쓸지 말지 모르겠음
+# # Function to encode the image
+# def encode_image(image_path):
+#   with open(image_path, "rb") as image_file:
+#     return base64.b64encode(image_file.read()).decode('utf-8')
+
+# # Path to your image
+# image_path = "path_to_your_image.jpg"
+
+# # Getting the base64 string
+# base64_image = encode_image(image_path)
+
+# headers = {
+#   "Content-Type": "application/json",
+#   "Authorization": f"Bearer {api_key}"
+# }
+
+# payload = {
+#   "model": "gpt-4-vision-preview",
+#   "messages": [
+#     {
+#       "role": "user",
+#       "content": [
+#         {
+#           "type": "text",
+#           "text": "What’s in this image?"
+#         },
+#         {
+#           "type": "image_url",
+#           "image_url": {
+#             "url": f"data:image/jpeg;base64,{base64_image}"
+#           }
+#         }
+#       ]
+#     }
+#   ],
+#   "max_tokens": 300
+# }
+
+# response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+# print(response.json())
