346aabdfe67e753cc650e9d46f8f534d4ae6e64f
kms1601 <kms_1601@naver.com>
2023-12-28 09:49:16 +0900

    [FE] 일정 서버에 전송하는 코드 수정

--- fillow/fillow_views.py
+++ fillow/fillow_views.py
@@ -312,17 +312,22 @@ def qna_details(request, id):
 
 def schedule(request):
     if request.method == "POST":
-        # DB에 올리는 코드
-        print(request.POST)
+        # JSON으로 수정된 일정 데이터 받아옴
+        received_data = json.loads(request.body.decode('utf-8'))
+        # DB에 올리는 코드 여기에 작성
+        print(received_data)
 
     context={
         "page_title":"일정 관리"
     }
     
     schedule_list = get_schedule()
+    # 달력에 배정 된 일정
     in_calendar = []
+    # 배정되지 않은 일정
     not_in_calendar = []
     
+    # 받아온 일정 리스트에서 start 속성이 없는(배정이 되지 않은) 일정 분리
     for schedule in schedule_list:
         if (schedule.get('start')):
             in_calendar.append(schedule)
